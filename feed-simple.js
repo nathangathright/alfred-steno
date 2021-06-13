175d2df6fc03ad2ae9f70cff4acfca5edf1fd71a
Nathan Gathright <nathan.gathright@gmail.com>
2021-06-12 19:24:27 -0500

    remove gitignore files

--- feed-simple.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const alfy = require('alfy');
-const basedFeed = Buffer.from(alfy.input).toString('base64').replace('/', '_').replace('+', '-');
-
-alfy.output([{
-    title: 'Open feed in Steno.fm',
-    arg: basedFeed
-}]);
